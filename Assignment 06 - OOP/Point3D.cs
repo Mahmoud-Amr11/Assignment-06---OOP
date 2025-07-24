fe4f11e312d097b7f394af352d8fc23ea069a528
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-07-24 17:27:17 +0300

    First Project

---
+++ Assignment 06 - OOP/Point3D.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment_06___OOP
+{
+    public class Point3D: IComparable<Point3D> , ICloneable
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Point3D()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+        public Point3D(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Z = 0;
+        }
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public int CompareTo(Point3D? other)
+        {
+            return (X, Y).CompareTo((other?.X ?? 0, other?.Y ?? 0));
+        }
+      
+        public object Clone()
+        {
+            return new Point3D(this.X, this.Y, this.Z);
+        }
+    }
+
+}
--- Assignment 06 - OOP/Program.cs
+++ Assignment 06 - OOP/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace Assignment_06___OOP
+﻿using System.Drawing;
+
+namespace Assignment_06___OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region First Project:
+
+            //Override the ToString Function to produce this output:   Point3D P = new Point3D(10, 10, 10);          Console.WriteLine(P.ToString());    Output: “Point Coordinates: (10, 10, 10)”.
+
+            Point3D P1 = new Point3D(10, 10, 10);
+            Console.WriteLine(P1.ToString());
+
+
+            //Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+            Point3D P2 = new Point3D();
+            int x;
+            int.TryParse(Console.ReadLine(), out x);
+            P2.X = x;
+            P2.Y = int.Parse(Console.ReadLine());
+
+
+
+            //Try to use == If(P1 == P2)   Does it work properly? 
+
+            if (P1 == P2)
+            {
+                Console.WriteLine("The two points are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The two points are not equal.");
+            }
+            // will compare the two points reference not the values
+
+
+
+
+            //Define an array of points and sort this array based on X & Y coordinates.
+            Point3D[] points ={
+                new Point3D(7, 8, 9),
+                new Point3D(1, 2, 20),
+                new Point3D(10, 11, 12),
+                new Point3D(4, 5, 6),
+                new Point3D(13, 14, 15)
+            };
+            Array.Sort(points);
+
+            foreach (var point in points)
+            {
+                Console.WriteLine(point.ToString());
+            }
+
+
+
+            #endregion
         }
     }
+
 }
